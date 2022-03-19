3b2b8fb3ffd6bd7d4ed6ab2fc02cffe9139e7ec0
Ellia Bagoes Setyo <bagoes.tyo@gmail.com>
2022-03-19 18:19:16 +0700

    Update verify Token

--- utils/schema.js
+++ utils/schema.js
@@ -2,7 +2,7 @@
 const Joi = require("joi");
 
 const schema = Joi.object({
-  username: Joi.string().alphanum().min(6).max(10).required(),
+  username: Joi.string().alphanum().min(6).max(50).required(),
   email: Joi.string().email().required(),
   password: Joi.string().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")).required(),
   fullname: Joi.string().required(),
